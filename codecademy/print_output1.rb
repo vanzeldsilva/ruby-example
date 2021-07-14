53f91d500902305ff661979e7cdb1053fc2d667a
vanzeldsilva <vanzeldsilva@gmail.com>
2021-07-14 14:34:23 +0530

    printing output1

---
+++ codecademy/print_output1.rb
@@ -0,0 +1,11 @@
+first_name = "vanzel"
+puts "Your name is #{first_name}!"
+
+last_name = "dsilva"
+puts "Your last name is #{last_name}!"
+
+city = "vasai"
+puts "Your city is #{city}!"
+
+state = "maharashtra"
+puts "Your state is #{state}!"
