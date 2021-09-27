e6a121e1098e218decb130697ed4096328e69926
vanzeldsilva <vanzeldsilva@gmail.com>
2021-09-27 17:57:27 +0530

    enumeration select pattern question

--- enumerables/exercises_1/spec/select_pattern_spec.rb
+++ enumerables/exercises_1/spec/select_pattern_spec.rb
@@ -79,19 +79,23 @@ RSpec.describe 'select pattern' do
     expect(dinosaurs).to eq(["tyrannosaurus", "achillesaurus", "qingxiusaurus"])
   end
 
-  xit 'floats' do
+  it 'floats' do
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
     # Your code goes here
+    floats = []
+    numbers.each do |number|
+      floats << number if ('%.2f' % number)
+    end
     expect(floats).to eq([1.4, 3.5, 4.9, 9.1, 8.0])
   end
 
-  xit 'arrays' do
+  it 'arrays' do
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
     # Your code goes here
     expect(arrays).to eq([["dog"], [56, 3, 8]])
   end
 
-  xit 'hashes' do
+  it 'hashes' do
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
     # Your code goes here
     expect(hashes).to eq([{:dog=>"fido"}, {:stuff=>"things"}])
