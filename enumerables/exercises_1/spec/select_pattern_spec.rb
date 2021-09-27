b5d70ca3600acb06ef3e2dabcb61d79ca25a6192
vanzeldsilva <vanzeldsilva@gmail.com>
2021-09-27 17:48:42 +0530

    enumeration select pattern done

--- enumerables/exercises_1/spec/select_pattern_spec.rb
+++ enumerables/exercises_1/spec/select_pattern_spec.rb
@@ -9,49 +9,73 @@ RSpec.describe 'select pattern' do
     expect(evens).to eq([2, 4, 6, 8, 10])
   end
 
-  xit 'picks odd numbers' do
+  it 'picks odd numbers' do
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     odds = []
     numbers.each do |number|
       # Your code goes here
+      odds << number if number.odd?
     end
     expect(odds).to eq([1, 3, 5, 7, 9])
   end
 
-  xit 'words with three letters' do
+  it 'words with three letters' do
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
     selected = []
     # Your code goes here
+    words.each do |word|
+      selected << word if word.length == 3
+    end
     expect(selected).to eq(["bad", "cat", "dog", "red"])
   end
 
-  xit 'words with more than three letters' do
+  it 'words with more than three letters' do
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
     # Your code goes here
+    selected = []
+    words.each do |word|
+        selected << word if word.length > 3
+    end
     expect(selected).to eq(["pill", "finger", "blue", "table"])
   end
 
-  xit 'words ending in e' do
+  it 'words ending in e' do
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
     # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word.end_with?('e')
+  end
     expect(selected).to eq(["are", "strike", "piece", "warble", "pipe"])
   end
 
-  xit 'words ending in ing' do
+  it 'words ending in ing' do
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
     # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word.end_with?('ing')
+    end
     expect(selected).to eq(["bring", "singing"])
   end
 
-  xit 'words containing e' do
+  it 'words containing e' do
     words = ["four", "red", "five", "blue", "pizza", "purple"]
     # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word.include?('e')
+    end
     expect(selected).to eq(["red", "five", "blue", "purple"])
   end
 
-  xit 'dinosaurs' do
+  it 'dinosaurs' do
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
     # Your code goes here
+    dinosaurs = []
+    animals.each do |animal|
+      dinosaurs << animal if animal.include?('saurus')
+    end
     expect(dinosaurs).to eq(["tyrannosaurus", "achillesaurus", "qingxiusaurus"])
   end
 
