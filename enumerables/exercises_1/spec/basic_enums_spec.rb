41e4ceaf1b67c9e99daf644fa38f501b641751ce
vanzeldsilva <vanzeldsilva@gmail.com>
2021-09-28 22:18:48 +0530

    enumeration basic done

--- enumerables/exercises_1/spec/basic_enums_spec.rb
+++ enumerables/exercises_1/spec/basic_enums_spec.rb
@@ -1,9 +1,8 @@
 RSpec.describe 'Basic Enums Test' do
   it 'solve for x' do
-    s == 'x'
-    5.times do
+    s = 'x'
       # write code here
-    end
+      4.times { s.concat('x') }
     expect(s).to eq('xxxxx')
   end
 
@@ -11,6 +10,9 @@ RSpec.describe 'Basic Enums Test' do
     sum = 0
     numbers = [1, 2, 3, 4, 5]
     # write code here
+    numbers.each do |num|
+    sum = sum + num
+  end
     expect(sum).to eq(15)
   end
 end
