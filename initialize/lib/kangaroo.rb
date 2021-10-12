3387b5c4bb28781b43e4bb69a46926bbde418f28
vanzeldsilva <vanzeldsilva@gmail.com>
2021-10-12 16:17:45 +0530

    for now

---
+++ initialize/lib/kangaroo.rb
@@ -0,0 +1,7 @@
+class Kangaroo
+  attr_reader :name, :children
+  def initialize(name, children)
+    @name = name
+    @children = children
+  end
+end
---
+++ initialize/lib/quail.rb
@@ -0,0 +1,6 @@
+class Quail
+  attr_reader :name
+  def initialize(name)
+    @name = name.capitalize
+  end
+end
